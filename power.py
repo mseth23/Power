dfb537d386c73f1c8d036b3a462b1957163cf0eb
Mihir Seth <Anish23@Anishs-MacBook-Pro-2.local>
2016-03-11 15:58:34 -0500

    power

---
+++ power.py
@@ -0,0 +1,9 @@
+def power(base,exponent):
+	product = 1
+	while exponent in range(exponent):
+		print("What do you want to multiply? ")
+		print("What power do you want to raise that to? ")
+		userResponse = input()
+		print(base * exponent)
+	return product
+print(power (3,4))
